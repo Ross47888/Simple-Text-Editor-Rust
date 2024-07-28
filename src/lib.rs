8da0353a7c1a66b34c1e96de568a880a2a10b07c
Ross47888 <rgilmore688@gmail.com>
2024-07-28 21:48:28 +0100

    Prints to user

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,36 @@
-//use std::fs;
+use std::path::PathBuf;
+use std::fs::File;
+use std::io::{Read, BufReader};
+
+use colored::Colorize;
 use clap::Parser;
 
 #[derive(Parser)]
 pub struct Cli {
-    pub path: String,
+    pub path: PathBuf,
+}
+
+pub fn driver_loop(file: &mut File) {
+    let mut buf = String::new();
+    let file_string_result = file.read_to_string(&mut buf);
+    let _file_string = match file_string_result {
+        Ok(file_string) => file_string,
+        Err(e) => panic!("Error reading file to string\n{:?}", e),
+    };
+
+    let title = "\nFile Contents".bold().blue();
+    println!("{}", title);
+    // println!("{}", {&buf});
+
+    let vec: Vec<&str> = buf.lines().collect();
+
+    for (c, v) in vec.iter().enumerate() {
+        println!("{} {}", c.to_string().bold(), v);
+    }
 }
 
 #[cfg(test)]
 mod tests {
     // use crate::lib::*;
-    use super::*;
 
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,19 @@
+use std::fs::File;
 use clap::Parser;
+
 use text_editor::*;
 
 fn main() {
     let args = Cli::parse();
     println!("{:?}", args.path);
+
+    let file_result = File::open(args.path);
+
+    let mut file = match file_result {
+        Ok(file) => file,
+        Err(e) => panic!("Failed to read your file!\n{:?}", e),
+    };
+    
+    println!("{:?}", file);
+    driver_loop(&mut file);
 }
